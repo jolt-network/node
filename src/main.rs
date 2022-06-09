1b2aae55cc7c37ddade06664c468e84167b368d5
Federico Luzzi <fedeluzzi00@gmail.com>
2022-06-09 21:06:58 +0200

    feat: new abi, address and naming

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 use std::str::FromStr;
 use std::sync::Arc;
 
-abigen!(Master, "abis/master.json");
+abigen!(Jolt, "abis/jolt.json");
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -24,10 +24,10 @@ struct Args {
     private_key: String,
 }
 
-static MASTER_ADDRESS: Lazy<HashMap<U256, Address>> = Lazy::new(|| {
+static JOLT_ADDRESS: Lazy<HashMap<U256, Address>> = Lazy::new(|| {
     [(
-        Chain::Rinkeby.into(),
-        Address::from_str("85B931f4fEF1bCb00a550862d6c7890b4652f04E").expect("Decoding failed"),
+        Chain::Goerli.into(),
+        Address::from_str("d5192f7DB2c20764aa66336F61f711e3Fe9CC43C").expect("Decoding failed"),
     )]
     .into()
 });
@@ -55,9 +55,9 @@ async fn main() -> eyre::Result<()> {
     // Getting multicall contracts
     let mut multicall = Multicall::new(client.clone(), None).await?;
 
-    // Getting master contract
-    let master = Master::new(
-        MASTER_ADDRESS
+    // Getting Jolt contract
+    let jolt = Jolt::new(
+        JOLT_ADDRESS
             .get(&chain_id)
             .expect("Invalid chain id")
             .clone(),
@@ -67,18 +67,19 @@ async fn main() -> eyre::Result<()> {
     while let Some(block) = websocket_provider.subscribe_blocks().await?.next().await {
         let block_number = U256::from(block.number.unwrap().as_u64());
 
-        let jobs = fetch_jobs(&master).await?;
+        let jobs = fetch_jobs(&jolt).await?;
         let workable_jobs: Vec<(&JobInfo, Bytes)> =
-            get_workable_jobs(&mut multicall, &master, &jobs).await?;
+            get_workable_jobs(&mut multicall, &jolt, &jobs).await?;
 
         println!(
-            "Workable jobs at block {}: {}",
+            "Workable jobs at block {}: {}/{}",
             block_number,
-            workable_jobs.len()
+            workable_jobs.len(),
+            jobs.len()
         );
 
         for (job, bytes) in workable_jobs {
-            let contract_call = master.work(job.id, bytes.clone());
+            let contract_call = jolt.work(job.id, bytes.clone());
             let pending_tx: PendingTransaction<Http> = contract_call.send().await?;
             dbg!(pending_tx.tx_hash());
             pending_tx.await?;
@@ -89,10 +90,10 @@ async fn main() -> eyre::Result<()> {
 }
 
 async fn fetch_jobs(
-    master: &Master<SignerMiddleware<Provider<Http>, LocalWallet>>,
+    jolt: &Jolt<SignerMiddleware<Provider<Http>, LocalWallet>>,
 ) -> eyre::Result<Vec<JobInfo>> {
-    let jobs_amount = master.jobs_amount().call().await?;
-    Ok(master
+    let jobs_amount = jolt.jobs_amount().call().await?;
+    Ok(jolt
         .jobs_slice(U256::from(0 as u32), jobs_amount)
         .call()
         .await?)
@@ -100,12 +101,12 @@ async fn fetch_jobs(
 
 async fn get_workable_jobs<'a>(
     multicall: &'a mut Multicall<SignerMiddleware<Provider<Http>, LocalWallet>>,
-    master: &'a Master<SignerMiddleware<Provider<Http>, LocalWallet>>,
+    jolt: &'a Jolt<SignerMiddleware<Provider<Http>, LocalWallet>>,
     jobs: &'a Vec<JobInfo>,
 ) -> eyre::Result<Vec<(&'a JobInfo, Bytes)>> {
     multicall.clear_calls();
     for job in jobs {
-        multicall.add_call(master.workable(master.client().address(), job.id));
+        multicall.add_call(jolt.workable(jolt.client().address(), job.id));
     }
     Ok(multicall
         .call_raw()
